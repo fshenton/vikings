4dcbabe9de2d64b7bcd56cb267135fdcce8fea2d
Frank Shenton <fshenton@gmail.com>
2019-09-02 14:05:09 +0100

    Added more appropriate tags to EpisodePreview render.

--- src/components/EpisodePreview/EpisodePreview.jsx
+++ src/components/EpisodePreview/EpisodePreview.jsx
@@ -3,18 +3,17 @@ import React from "react";
 export default function EpisodePreview(){
 
 	return (
-		<p>
-			Preview!
-		</p>
+		<article>
+			<button>Close</button>
+			<img 
+				src="path/to/image.jpg" 
+				alt="placeholder description" 
+			/>
+			<video controls src="path/to/video.mp4" />
+			<h2>Episode X</h2>
+			<h1>Name of Episode X</h1>
+			<button>Previous</button>
+			<button>Next</button>
+		</article>
 	);
-
-	/*
-	close button
-	thumbnail image during loading
-	video
-	buffering loading animation
-	play/pause toggle button
-	h2
-	h1
-	prev/next buttons
 }
