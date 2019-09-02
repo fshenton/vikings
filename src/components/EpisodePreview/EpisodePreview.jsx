55a63ea13221e5b0b270085cc906c7c689bf9874
Frank Shenton <fshenton@gmail.com>
2019-09-02 14:53:10 +0100

    Changed EpisodePreview so that it returns a form instead of an article as that seems more semantically appropriate.

--- src/components/EpisodePreview/EpisodePreview.jsx
+++ src/components/EpisodePreview/EpisodePreview.jsx
@@ -3,17 +3,19 @@ import React from "react";
 export default function EpisodePreview(){
 
 	return (
-		<article>
+		<form>
 			<button>Close</button>
-			<img 
-				src="path/to/image.jpg" 
-				alt="placeholder description" 
-			/>
-			<video controls src="path/to/video.mp4" />
+			<output>
+				<img 
+					src="path/to/image.jpg" 
+					alt="placeholder description" 
+				/>
+				<video controls src="path/to/video.mp4" />
+			</output>
 			<h2>Episode X</h2>
 			<h1>Name of Episode X</h1>
 			<button>Previous</button>
 			<button>Next</button>
-		</article>
+		</form>
 	);
 }
