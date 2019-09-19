1b7b0338c7a7eed5d473c4ea78b9df6272e669a1
fshenton <fshenton@gmail.com>
2019-09-19 16:54:05 +0100

    Added styles to Characters. Sorting some import ordering.

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,8 +1,9 @@
 import RENDER from "./renderUtils.jsx";
+import * as ACTIONS from "./actions.js";
 import data from "./data.js";
 import { CharactersContext, CharactersProvider } from "./context.jsx";
 import reducer from "./reducer.js";
-import * as ACTIONS from "./actions.js";
+import s from "./styles.scss";
 
 export { default } from "./Characters.jsx";
-export { RENDER, ACTIONS, data, reducer, CharactersContext, CharactersProvider };
+export { RENDER, ACTIONS, data, CharactersContext, CharactersProvider, reducer, s };
