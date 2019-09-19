95ce3b67bac2b69c8f510aadd7aa5be486efcb42
fshenton <fshenton@gmail.com>
2019-09-19 13:35:34 +0100

    Added font sizes, spacing and flex.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,17 +3,17 @@ import { s } from "./";
 
 export default function Landing(){
 	return(
-		<header>
-			<h1>
+		<header className={ s.wrapper }>
+			<h1 className={ s.heading }>
 				Vikings
 			</h1>
-			<h2>
+			<h2 className={ s.subheading }>
 				Descend into Darkness
 			</h2>
-			<p>
+			<p className={ s.body }>
 				When a king falls, another will rise. But who will it be?
 			</p>
-			<p>
+			<p className={ s.body }>
 				Get a closer look at <span>Vikings season 5</span>.
 			</p>
 		</header>
