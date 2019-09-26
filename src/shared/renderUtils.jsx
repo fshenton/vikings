91d6557e346663b116dd32fe78fb2c84fafd8d2e
fshenton <fshenton@gmail.com>
2019-09-26 14:04:11 +0100

    Changed body render function so it uses scope not component, and now binding the map function call to pass it in.

--- src/shared/renderUtils.jsx
+++ src/shared/renderUtils.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-function renderBody(body, component){
+function renderBody(body, scope = ""){
 	const paragraphs = body.split("\n"); // splits the string at every line-break
-	return paragraphs.map(renderParagraph);
+	
+	return paragraphs.map(
+		renderParagraph.bind(true, scope)
+	);
 }// renderBody
 
-function renderParagraph(text, index, component){
-	const key = `${component}__body__paragraph_${index}`;
+function renderParagraph(scope, text, index){
+	const key = `${scope}__body__paragraph_${index}`;
+	
 	return (
 		<p key={ key }>
 			{ text }
