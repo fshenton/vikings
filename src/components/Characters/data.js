d5c1d4516bb63eee3ffc34748bb063faccae6e2c
fshenton <fshenton@gmail.com>
2019-10-01 14:13:19 +0100

    Mobile landscape basics.

--- src/components/Characters/data.js
+++ src/components/Characters/data.js
@@ -3,7 +3,8 @@ export default [
 		name: "Ivar",
 		nickname: "The Boneless",
 		actor: "Alex Hogh Andersen",
-		body: "Ivar Ragnarsson, named Ivar the Boneless by his father (due to his disease), is the youngest son of Ragnar Lothbrok and Aslaug. He swears revenge on Lagertha for killing his mother and against Kings Aelle and Ecbert for Ragnar's death.",
+		body: `Ivar Ragnarsson, named Ivar the Boneless by his father (due to his disease), is the youngest son of Ragnar Lothbrok and Aslaug. He swears revenge on Lagertha for killing his mother and against Kings Aelle and Ecbert for Ragnar's death. 
+		Ivar Ragnarsson, named Ivar the Boneless by his father (due to his disease), is the youngest son of Ragnar Lothbrok and Aslaug. He swears revenge on Lagertha for killing his mother and against Kings Aelle and Ecbert for Ragnar's death.`,
 		image: {
 			src: "path/to/ivar/portrait.jpg",
 			mask: "path/to/ivar/mask.png",
