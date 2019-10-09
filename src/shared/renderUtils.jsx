c04a7aab035a736a16d4b9a60629796294471886
fshenton <fshenton@gmail.com>
2019-10-09 10:21:08 +0100

    synced SHARED/renderUtils so we can use updated renderBody

--- src/shared/renderUtils.jsx
+++ src/shared/renderUtils.jsx
@@ -1,18 +1,57 @@
 import React from "react";
 
-function renderBody(body){
-	const paragraphs = body.split("\n"); // splits the string at every line-break
-	return paragraphs.map(renderParagraph);
+function renderBody(data, options = {}){ 
+	const paragraphs = data.split("\n"); // splits the string at every line-break
+	const customisedRenderParagraph = renderParagraph.bind(this, options);
+
+	return paragraphs.map(customisedRenderParagraph);
 }// renderBody
 
-function renderParagraph(text, index){
-	const key = `about__body__paragraph_${index}`;
+
+function renderParagraph(options, text, index){
+	const { scope, className } = options;
+
+	const key = `${scope}__body__paragraph_${index}`;
+	
+	//if there is a link inside the text, cut it out
+	const link = findLink(text) || {};
+	if(link) text = text.slice(0, link.index);
+
 	return (
-		<p key={ key }>
+		<p 
+			className={ className }
+			key={ key }
+		>
 			{ text }
+			
+			{ link && (
+				<a href={ link.destination } >
+					{ link.text }
+				</a>
+			)}
 		</p>
 	);
 }// renderParagraph
+
+
+function findLink(markdown){
+	const labelStart = markdown.indexOf("[");
+
+	if(labelStart === -1) return false; //no link found
+
+	const labelEnd     = markdown.indexOf("]", labelStart);
+	const text         = markdown.substring(labelStart+1, labelEnd);
+	
+	const destStart    = labelEnd+2; 
+	const destEnd      = markdown.indexOf(")", destStart);
+	const destination  = markdown.substring(destStart, destEnd);
+
+	return {
+		index: labelStart,
+		text,
+		destination
+	};
+}// findLink
 
 export default {
 	body: renderBody
