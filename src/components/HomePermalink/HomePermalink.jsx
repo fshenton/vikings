be36a6ed2296634fcde80965eb81f9cf86f05508
fshenton <fshenton@gmail.com>
2019-10-17 14:16:33 +0100

    removed isSmall usage from HomePermalink.

--- src/components/HomePermalink/HomePermalink.jsx
+++ src/components/HomePermalink/HomePermalink.jsx
@@ -1,18 +1,10 @@
 import React, { useContext } from "react";
 import Link from "COMPONENTS/Link/";
-import { ClientContext as Client } from "COMPONENTS/App/";
 import { s } from "./";
 
 export default function HomePermalink(){
 
-	const {
-		isSmall
-	} = useContext(Client).state;
-
-	console.log(isSmall);
-
 	return (
-		!isSmall && 
 			<Link
 				className={ s.home } 
 				destination="/"
