aa042a67e86597c51798410bf2496d4830a135b7
fshenton <fshenton@gmail.com>
2019-10-21 11:40:18 +0100

    removed the Client components, leaving just the context and reducer. Provider now wraps App in main index.js.

--- src/components/Client/index.js
+++ src/components/Client/index.js
@@ -3,4 +3,3 @@ import * as ACTIONS from "./actions.js";
 import { ClientContext, ClientProvider } from "./context.jsx";
 
 export { reducer, ACTIONS, ClientContext, ClientProvider };
-export { default } from "./Client.jsx";
