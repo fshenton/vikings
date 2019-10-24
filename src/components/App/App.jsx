96eff623ad06f517d7796cdb827c12accbab9106
fshenton <fshenton@gmail.com>
2019-10-24 11:32:27 +0100

    removed unused import.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import HomePermalink from "COMPONENTS/HomePermalink/";
 import HistoryChannel from "COMPONENTS/HistoryChannel/";
 import WatchNow from "COMPONENTS/WatchNow/";
