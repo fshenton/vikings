d4ac9b95b35ec4c36ef7b48875b4c3e95d11f53a
fshenton <fshenton@gmail.com>
2019-11-18 13:46:56 +0000

    refactored logoartwork to use data and renderUtils as in EpisodeNav etc. Also replaced all of the assets, now we have 14 bone parts. To be animated.

--- src/components/LogoArtwork/index.js
+++ src/components/LogoArtwork/index.js
@@ -1,4 +1,8 @@
 import s from "./styles.scss";
+import RENDER from "./renderUtils.jsx";
+import data from "./data.js";
 
-export { s };
+console.log(RENDER);
+
+export { RENDER, data, s };
 export { default } from "./LogoArtwork.jsx";
