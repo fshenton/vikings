1fddad4d6bbfdfd07d101060e3b9f8fefe254f05
fshenton <fshenton@gmail.com>
2019-11-19 17:03:44 +0000

    started working on svg outline animation. svg positions make my head go to mush.

--- src/components/HistoryChannel/HistoryChannel.jsx
+++ src/components/HistoryChannel/HistoryChannel.jsx
@@ -20,7 +20,17 @@ export default function HistoryChannel(){
 				className={ s.history }
 				destination="https://www.history.co.uk/"
 				aria-label="The History Channel"
-			/>
+			>
+				<svg width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
+					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="4"
+					d="
+				        M 50, 50
+				        m -50, 0
+				        a 50,50 0 1,0 100,0
+				        a 50,50 0 1,0 -100,0
+				    "/>
+				</svg>
+			</Link>
 		);
 	}
 }// HistoryChannel
