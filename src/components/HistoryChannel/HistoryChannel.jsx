9782f8aae856aa57d50a17d8d91e1290894c5bd5
fshenton <fshenton@gmail.com>
2019-11-20 11:21:28 +0000

    added basic active effects to watch now and history channel.

--- src/components/HistoryChannel/HistoryChannel.jsx
+++ src/components/HistoryChannel/HistoryChannel.jsx
@@ -23,12 +23,13 @@ export default function HistoryChannel(){
 			>
 				<svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
 					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="2"
-					d="
-				        M 50, 50
-				        m -48, 0
-				        a 48,48 0 1,0 96,0
-				        a 48,48 0 1,0 -96,0
-				    "/>
+						d="
+					        M 50, 50
+					        m -48, 0
+					        a 48,48 0 1,0 96,0
+					        a 48,48 0 1,0 -96,0
+					    "
+					/>
 				</svg>
 			</Link>
 		);
