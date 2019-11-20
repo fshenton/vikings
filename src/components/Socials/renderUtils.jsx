1940cd2f24288edf2e883e36feb84b2f2c69c9b9
fshenton <fshenton@gmail.com>
2019-11-20 11:04:28 +0000

    copied circling animation to socials.

--- src/components/Socials/renderUtils.jsx
+++ src/components/Socials/renderUtils.jsx
@@ -23,7 +23,17 @@ function renderItem(data){
 				aria-label={ label }
 				destination={ destination } 
 				{ ...remainingProps }
-			/>
+			>
+				<svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
+					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="2"
+					d="
+				        M 50, 50
+				        m -48, 0
+				        a 48,48 0 1,0 96,0
+				        a 48,48 0 1,0 -96,0
+				    "/>
+				</svg>
+			</Link>
 		</li>
 	);
 }// renderItem
