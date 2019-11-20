331245bd52d7015f0fd56bc52ca6833a4c04c767
fshenton <fshenton@gmail.com>
2019-11-20 10:59:01 +0000

    improved history channel circle animation.

--- src/components/HistoryChannel/HistoryChannel.jsx
+++ src/components/HistoryChannel/HistoryChannel.jsx
@@ -21,13 +21,13 @@ export default function HistoryChannel(){
 				destination="https://www.history.co.uk/"
 				aria-label="The History Channel"
 			>
-				<svg width="100px" height="100px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
-					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="4"
+				<svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
+					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="2"
 					d="
 				        M 50, 50
-				        m -50, 0
-				        a 50,50 0 1,0 100,0
-				        a 50,50 0 1,0 -100,0
+				        m -48, 0
+				        a 48,48 0 1,0 96,0
+				        a 48,48 0 1,0 -96,0
 				    "/>
 				</svg>
 			</Link>
