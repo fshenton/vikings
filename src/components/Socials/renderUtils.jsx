6ac711c4f5bae5f8df1e72b74676d9a076b4c797
fshenton <fshenton@gmail.com>
2019-11-24 12:11:31 +0000

    updated home logo and social svg colours to match other site elements.

--- src/components/Socials/renderUtils.jsx
+++ src/components/Socials/renderUtils.jsx
@@ -26,12 +26,13 @@ function renderItem(data){
 			>
 				<svg viewBox="0 0 100 100" preserveAspectRatio="xMidYMin">
 					<path className={ s.circle } fill="none" stroke="#FFF" strokeWidth="2"
-					d="
-				        M 50, 50
-				        m -48, 0
-				        a 48,48 0 1,0 96,0
-				        a 48,48 0 1,0 -96,0
-				    "/>
+						d="
+					        M 50, 50
+					        m 0, -49
+					        a 49,49 0 1,0 0,98
+					        a -49,-49 0 1,0 0,-98
+					    "
+				    />
 				</svg>
 			</Link>
 		</li>
