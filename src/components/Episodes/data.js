f26248572c6df4473ae2bb1f6b655356fcd62bdb
fshenton <fshenton@gmail.com>
2019-12-03 14:08:53 +0000

    added ghost images as thumbs for 5 of the episodes. Reduced overall episode count to6 as only 6 assets provided.

--- src/components/Episodes/data.js
+++ src/components/Episodes/data.js
@@ -3,9 +3,9 @@ export default [
 		number: 1,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep1-small.jpg",
+				medium: "/assets/2d/episodes/ep1-medium.jpg",
+				large: "/assets/2d/episodes/ep1-large.jpg"
 			},
 			description: "Man looks on, concerned."
 		},
@@ -13,7 +13,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep1-large.jpg",
 		},
 		title: "The Fisher King",
 		synopsis: "Guided by Bishop Heahmund's visions, the Saxons devise a battle plan. Ivar the Boneless strategizes on his own as the Vikings undergo increasing pressure. Bjorn Ironside finds himself in a new territory and must convince the local commander he is a trader and not a raider."
@@ -21,9 +21,9 @@ export default [
 		number: 2,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep2-small.jpg",
+				medium: "/assets/2d/episodes/ep2-medium.jpg",
+				large: "/assets/2d/episodes/ep2-large.jpg"
 			},
 			description: "Man dramatically looks out at the sea."
 		},
@@ -31,7 +31,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep2-large.jpg",
 		},
 		title: "The Departed",
 		synopsis: "As the Vikings establish their stronghold in York, Ivar is determined to lead the Great Army. In Kattegat, Lagertha misjudges King Harald and pays a heavy price. Floki arrives at a mysterious land."
@@ -39,9 +39,9 @@ export default [
 		number: 3,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep3-small.jpg",
+				medium: "/assets/2d/episodes/ep3-medium.jpg",
+				large: "/assets/2d/episodes/ep3-large.jpg"
 			},
 			description: "Man yells angrily."
 		},
@@ -49,7 +49,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep3-large.jpg",
 		},
 		title: "Homeland",
 		synopsis: "Celebrations are cut short in the aftermath of the battle at York; Ragnar Lothbrok's sons are pitted against each other as tensions reach an all-time high, and each one is forced to choose a side."
@@ -58,9 +58,9 @@ export default [
 		number: 4,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep4-small.jpg",
+				medium: "/assets/2d/episodes/ep4-medium.jpg",
+				large: "/assets/2d/episodes/ep4-large.jpg"
 			},
 			description: "Description of episode 4 thumbnail."
 		},
@@ -68,7 +68,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep4-large.jpg",
 		},
 		title: "The Plan",
 		synopsis: "Guided by Bishop Heahmund's visions, the Saxons devise a battle plan. Ivar the Boneless strategizes on his own as the Vikings undergo increasing pressure. Bjorn Ironside finds himself in a new territory and must convince the local commander he is a trader and not a raider."
@@ -77,9 +77,9 @@ export default [
 		number: 5,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep5-small.jpg",
+				medium: "/assets/2d/episodes/ep5-medium.jpg",
+				large: "/assets/2d/episodes/ep5-large.jpg"
 			},
 			description: "Description of episode 5 thumbnail."
 		},
@@ -87,7 +87,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep5-large.jpg",
 		},
 		title: "The Prisoner",
 		synopsis: "Ivar meets his match in Bishop Heahmund. Floki returns to Kattegat. Bjorn receives a lavish welcome in North Africa."
@@ -95,9 +95,9 @@ export default [
 		number: 6,
 		thumbnail: {
 			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
+				small: "/assets/2d/episodes/ep6-small.jpg",
+				medium: "/assets/2d/episodes/ep6-medium.jpg",
+				large: "/assets/2d/episodes/ep6-large.jpg"
 			},
 			description: "Description of episode 6 thumbnail."
 		},
@@ -105,87 +105,89 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
+			src: "/assets/2d/episodes/ep6-large.jpg",
 		},
 		title: "The Message",
 		synopsis: "Lagertha is betrayed, and Bjorn must find a way to support the distraught queen; Floki and his settlers arrive in a new land, but it is far from what they expected."
 	}
-	, {
-		number: 7,
-		thumbnail: {
-			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
-			},
-			description: "Description of episode 7 thumbnail."
-		},
-		trailer: {
-			src: "/assets/video/vikings-large.mp4"
-		},
-		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
-		},
-		title: "Full Moon",
-		synopsis: "Bjorn returns to Kattegat to learn that an attack will take place during the next full moon; Ivar must decide if he can place his trust in a former enemy on the battlefield."
-	}
-	, {
-		number: 8,
-		thumbnail: {
-			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
-			},
-			description: "Description of episode 8 thumbnail."
-		},
-		trailer: {
-			src: "/assets/video/vikings-large.mp4"
-		},
-		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
-		},
-		title: "The Joke",
-		synopsis: "The battle for Kattegat begins. As the two armies line up to fight, the Great Heathen Army must decide between a final plea for peace or all-out war. Floki faces discord amongst the settlers."
-	}
-	, {
-		number: 9,
-		thumbnail: {
-			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
-			},
-			description: "Description of episode 9 thumbnail."
-		},
-		trailer: {
-			src: "/assets/video/vikings-large.mp4"
-		},
-		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
-		},
-		title: "A Simple Story",
-		synopsis: "The army leaders consider their options in the aftermath of the battle. In Floki's camp, all hopes of binding the community together are dashed as tragedy unfolds."
-	}
-	, {
-		number: 10,
-		thumbnail: {
-			src: {
-				small: "/assets/2d/episodes/thumbnail-small.jpg",
-				medium: "/assets/2d/episodes/thumbnail-medium.jpg",
-				large: "/assets/2d/episodes/thumbnail-large.jpg"
-			},
-			description: "Description of episode 10 thumbnail."
-		},
-		trailer: {
-			src: "/assets/video/vikings-large.mp4"
-		},
-		poster: {
-			src: "/assets/2d/episodes/thumbnail-large.jpg",
-		},
-		title: "Moments of Vision",
-		synopsis: "A sense of doom looms over Kattegat as bloodshed ensues, but as the defeated army flees in the face of the victors, a legendary warrior makes his way home."
-	}
+
+	//NOT USED
+	// , {
+	// 	number: 7,
+	// 	thumbnail: {
+	// 		src: {
+	// 			small: "/assets/2d/episodes/thumbnail-small.jpg",
+	// 			medium: "/assets/2d/episodes/thumbnail-medium.jpg",
+	// 			large: "/assets/2d/episodes/thumbnail-large.jpg"
+	// 		},
+	// 		description: "Description of episode 7 thumbnail."
+	// 	},
+	// 	trailer: {
+	// 		src: "/assets/video/vikings-large.mp4"
+	// 	},
+	// 	poster: {
+	// 		src: "/assets/2d/episodes/thumbnail-large.jpg",
+	// 	},
+	// 	title: "Full Moon",
+	// 	synopsis: "Bjorn returns to Kattegat to learn that an attack will take place during the next full moon; Ivar must decide if he can place his trust in a former enemy on the battlefield."
+	// }
+	// , {
+	// 	number: 8,
+	// 	thumbnail: {
+	// 		src: {
+	// 			small: "/assets/2d/episodes/thumbnail-small.jpg",
+	// 			medium: "/assets/2d/episodes/thumbnail-medium.jpg",
+	// 			large: "/assets/2d/episodes/thumbnail-large.jpg"
+	// 		},
+	// 		description: "Description of episode 8 thumbnail."
+	// 	},
+	// 	trailer: {
+	// 		src: "/assets/video/vikings-large.mp4"
+	// 	},
+	// 	poster: {
+	// 		src: "/assets/2d/episodes/thumbnail-large.jpg",
+	// 	},
+	// 	title: "The Joke",
+	// 	synopsis: "The battle for Kattegat begins. As the two armies line up to fight, the Great Heathen Army must decide between a final plea for peace or all-out war. Floki faces discord amongst the settlers."
+	// }
+	// , {
+	// 	number: 9,
+	// 	thumbnail: {
+	// 		src: {
+	// 			small: "/assets/2d/episodes/thumbnail-small.jpg",
+	// 			medium: "/assets/2d/episodes/thumbnail-medium.jpg",
+	// 			large: "/assets/2d/episodes/thumbnail-large.jpg"
+	// 		},
+	// 		description: "Description of episode 9 thumbnail."
+	// 	},
+	// 	trailer: {
+	// 		src: "/assets/video/vikings-large.mp4"
+	// 	},
+	// 	poster: {
+	// 		src: "/assets/2d/episodes/thumbnail-large.jpg",
+	// 	},
+	// 	title: "A Simple Story",
+	// 	synopsis: "The army leaders consider their options in the aftermath of the battle. In Floki's camp, all hopes of binding the community together are dashed as tragedy unfolds."
+	// }
+	// , {
+	// 	number: 10,
+	// 	thumbnail: {
+	// 		src: {
+	// 			small: "/assets/2d/episodes/thumbnail-small.jpg",
+	// 			medium: "/assets/2d/episodes/thumbnail-medium.jpg",
+	// 			large: "/assets/2d/episodes/thumbnail-large.jpg"
+	// 		},
+	// 		description: "Description of episode 10 thumbnail."
+	// 	},
+	// 	trailer: {
+	// 		src: "/assets/video/vikings-large.mp4"
+	// 	},
+	// 	poster: {
+	// 		src: "/assets/2d/episodes/thumbnail-large.jpg",
+	// 	},
+	// 	title: "Moments of Vision",
+	// 	synopsis: "A sense of doom looms over Kattegat as bloodshed ensues, but as the defeated army flees in the face of the victors, a legendary warrior makes his way home."
+	// }
 ];
 
 
