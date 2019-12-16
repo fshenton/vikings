e81a0c8f43958dc345da7e50b2f151d07f21bb65
fshenton <fshenton@gmail.com>
2019-12-16 18:16:13 +0000

    added linebreak to ubbe copy.

--- src/components/Characters/data.js
+++ src/components/Characters/data.js
@@ -3,7 +3,7 @@ export default [
 		name: "Ivar",
 		nickname: "The Boneless",
 		actor: "Alex Hogh Andersen",
-		body: "Ivar Ragnarsson, named Ivar the Boneless by his father (due to his disease), is the youngest son of Ragnar Lothbrok and Aslaug. He swears revenge on Lagertha for killing his mother and against Kings Aelle and Ecbert for Ragnar’s death.",
+		body: `Ivar Ragnarsson, named Ivar the Boneless by his father (due to his disease), is the youngest son of Ragnar Lothbrok and Aslaug. He swears revenge on Lagertha for killing his mother and against Kings Aelle and Ecbert for Ragnar’s death.`,
 		image: {
 			src: { 
 				small: "./assets/2d/characters/ivar-portrait.jpg", 
@@ -21,7 +21,8 @@ export default [
 		name: "Ubbe",
 		nickname: "Ragnarsson",
 		actor: "Jordon Patrick Smith",
-		body: "Ubbe is the first son of Ragnar Lothbrok and Aslaug. He was conceived out of wedlock which would have made him a bastard, but with Ragnar’s marriage to Aslaug and divorce of Largetha he became Ragnar’s legitmate son and heir. He is the second oldest of Ragnar’s son and arguably the most mellow. He also closely resembles his father when he was a young man. He is married to Margrethe, but his lover is Torvi, who was previously his older brother, Bjorn’s wife.",
+		body: `Ubbe is the first son of Ragnar Lothbrok and Aslaug. He was conceived out of wedlock which would have made him a bastard, but with Ragnar’s marriage to Aslaug and divorce of Largetha he became Ragnar’s legitmate son and heir. 
+		He is the second oldest of Ragnar’s son and arguably the most mellow. He also closely resembles his father when he was a young man. He is married to Margrethe, but his lover is Torvi, who was previously his older brother, Bjorn’s wife.`,
 		image: {
 			src: { 
 				small: "./assets/2d/characters/ubbe-portrait.jpg", 
@@ -39,7 +40,7 @@ export default [
 		name: "Lagertha",
 		nickname: " ",
 		actor: "Katheryn Winnick",
-		body: "Lagertha is a respected warrior and the Queen of Kattegat. Since the loss of her children, Lagertha realizes that Freyja’s fertility is meant for another, and that it is the goddess’ warrior aspect that speaks to her fellow Valkyrie. Lagertha remains fiercely independent when it comes to protecting her family—and the throne.",
+		body: `Lagertha is a respected warrior and the Queen of Kattegat. Since the loss of her children, Lagertha realizes that Freyja’s fertility is meant for another, and that it is the goddess’ warrior aspect that speaks to her fellow Valkyrie. Lagertha remains fiercely independent when it comes to protecting her family—and the throne.`,
 		image: {
 			src: { 
 				small: "./assets/2d/characters/lagertha-portrait.jpg", 
