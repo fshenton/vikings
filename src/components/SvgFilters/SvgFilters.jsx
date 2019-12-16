e0872e1f64633aa96cdb707db957d3be27280f89
fshenton <fshenton@gmail.com>
2019-12-16 12:58:19 +0000

    made small adjustment to navigation link rope noise and width.

--- src/components/SvgFilters/SvgFilters.jsx
+++ src/components/SvgFilters/SvgFilters.jsx
@@ -4,10 +4,12 @@ import { s } from "./";
 export default function SvgFilters() {
 	return (
 		<svg className={ s.filters }> 
+			{/*CIRCLE*/}
 			<filter id="powder">
 				<feTurbulence baseFrequency="0.2" numOctaves="3"/>
 				<feDisplacementMap in="SourceGraphic" scale="5" />
 			</filter>
+			{/*LINE*/}
 			<filter 
 				id="rope"
 				filterUnits="userSpaceOnUse"
@@ -15,7 +17,7 @@ export default function SvgFilters() {
 	  			height="100%" 
 			>
 				<feTurbulence baseFrequency="0.2" numOctaves="3"/>
-				<feDisplacementMap in="SourceGraphic" scale="5" />
+				<feDisplacementMap in="SourceGraphic" scale="7" />
 			</filter>	
 		</svg>
 	);
