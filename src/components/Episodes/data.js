da48ab974223d122950863f0b32ee730b22339f3
fshenton <fshenton@gmail.com>
2019-12-16 17:05:10 +0000

    removed tap-highlight-color for mobile devices.

--- src/components/Episodes/data.js
+++ src/components/Episodes/data.js
@@ -13,7 +13,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep1-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "The Fisher King",
 		synopsis: "Guided by Bishop Heahmund's visions, the Saxons devise a battle plan. Ivar the Boneless strategizes on his own as the Vikings undergo increasing pressure. Bjorn Ironside finds himself in a new territory and must convince the local commander he is a trader and not a raider."
@@ -31,7 +31,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep2-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "The Departed",
 		synopsis: "As the Vikings establish their stronghold in York, Ivar is determined to lead the Great Army. In Kattegat, Lagertha misjudges King Harald and pays a heavy price. Floki arrives at a mysterious land."
@@ -49,7 +49,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep3-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "Homeland",
 		synopsis: "Celebrations are cut short in the aftermath of the battle at York; Ragnar Lothbrok's sons are pitted against each other as tensions reach an all-time high, and each one is forced to choose a side."
@@ -68,7 +68,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep4-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "The Plan",
 		synopsis: "Guided by Bishop Heahmund's visions, the Saxons devise a battle plan. Ivar the Boneless strategizes on his own as the Vikings undergo increasing pressure. Bjorn Ironside finds himself in a new territory and must convince the local commander he is a trader and not a raider."
@@ -87,7 +87,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep5-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "The Prisoner",
 		synopsis: "Ivar meets his match in Bishop Heahmund. Floki returns to Kattegat. Bjorn receives a lavish welcome in North Africa."
@@ -105,7 +105,7 @@ export default [
 			src: "/assets/video/vikings-large.mp4"
 		},
 		poster: {
-			src: "/assets/2d/episodes/ep6-large.jpg",
+			src: "/assets/2d/episodes/trailer-poster-large.jpg",
 		},
 		title: "The Message",
 		synopsis: "Lagertha is betrayed, and Bjorn must find a way to support the distraught queen; Floki and his settlers arrive in a new land, but it is far from what they expected."
