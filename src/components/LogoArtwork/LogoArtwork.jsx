9012573d4ed25e219e4545c60e59078cc01e61a9
fshenton <fshenton@gmail.com>
2019-12-19 17:37:13 +0000

    V logo now uses transitions instead of animations.

--- src/components/LogoArtwork/LogoArtwork.jsx
+++ src/components/LogoArtwork/LogoArtwork.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { RENDER, data, s } from "./";
 
 export default function LogoArtwork(props){
@@ -8,13 +8,28 @@ export default function LogoArtwork(props){
 
 	const { className } = props;
 
+	// STATE AND EFFECTS
+
+	const [active, setActive] = useState(false);
+
+	useEffect(fireTransition, []);
+
+	function fireTransition() {
+		const delay = setTimeout(() => {
+			setActive(true);
+		}
+		, 200);
+
+		return () => clearTimeout(delay);
+	}// fireTransition
+
 	//RENDER
 	//--------------------------
 
 	const images = data.map(RENDER.images);
 
 	return (
-		<div className={ `${s.wrapper} ${className}` }>
+		<div className={ `${s.wrapper} ${className} ${active ? s.active : s.inactive}` }>
 			{ images }
 		</div>
 	);
